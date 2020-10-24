e1f406731c2d4a9ef369d71830967ccb93e59353
michael <mpthornton86@gmail.com>
2020-10-24 12:42:41 -0600

    commit tree 7efc322c0a31278b1cfd5bbc12420e4febba80ba

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,9 @@
 require 'pry'
-
+#This is how .find?() works
 def my_find(collection)
-
+    x = 0
+    while x < collection.length
+        return collection[x] if yield(collection[x])
+        x += 1
+    end
 end
